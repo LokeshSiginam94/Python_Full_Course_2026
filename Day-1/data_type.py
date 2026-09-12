52ae2a055d3d94b0f1e3230111ca718acea44e32
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-12 16:18:27 +0530

    Add data_types

---
+++ Day-1/data_type.py
@@ -0,0 +1,14 @@
+name = "lokesh"
+age=22
+city="vja"
+shirt_price=1000.50
+
+print("my name is",name)
+print("my age is",age)
+print("my city is",city)
+print("my shirt price is",shirt_price)
+
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(shirt_price))
