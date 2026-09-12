f3aa70fd0128cba5787773769a364116e7752501
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-12 19:48:04 +0530

    Add I/O

---
+++ Day-1/Input_Output.py
@@ -0,0 +1,22 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+city = input("Enter your city:")
+
+print("Hello",name,"your age is",age,"and you live in",city)
+
+#or
+print("Hello",name)
+print("your age is",age)
+print("you live in",city)
+
+age=input("Enter your age:")
+print(type(age))
+
+#class <'str'>
+
+#correct way to convert string to integer
+Age=int(input("Enter your age:"))
+print(type(Age))
+
+#input() funtion always return string value and it takes the input from the use and stores it in the variable.
+#print() function is used to print the output on the screen.
