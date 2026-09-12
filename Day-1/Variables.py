3d8c6dc45b6f2b430ccd0096f83842b1bb4ea58b
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-12 16:04:15 +0530

    Add Variables

---
+++ Day-1/Variables.py
@@ -0,0 +1,40 @@
+name = "lokesh"
+age = 22
+city = "vja"
+language = "python"
+salary = 100000
+
+print("my name is",name)
+print("my age is",age)
+print("my city is",city)
+print("my favorite programming language is",language)
+print("my salary is",salary)
+
+# or All in same line
+
+print("my name is",name,"my age is",age,"my city is",city,"my favorite programming language is",language)
+
+#salary increased and age increases
+salary = 150000
+age = 23
+
+print("my name is",name)
+print("my age is",age)
+print("my city is",city)
+print("my favorite programming language is",language)
+print("my salary is",salary)
+
+#1name = "lokesh" wrong variable name
+#first name = "lokesh" space in invalid variable name
+#_variable_name =true
+#first_variable = true
+
+#python is case sensitive language
+
+Age=10
+age=20
+AGE=30
+
+print(Age)
+print(age)
+print(AGE)
