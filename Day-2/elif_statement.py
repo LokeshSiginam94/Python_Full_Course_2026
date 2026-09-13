47ccbba9e2ac056335dbdf9a3e205b1b461f6bfd
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-13 21:23:33 +0530

    Add elif_statement

---
+++ Day-2/elif_statement.py
@@ -0,0 +1,21 @@
+marks= int(input("Enter your marks:"))
+
+if marks>=90:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=50:
+    print("Grade C")
+else:
+    print("FAIL")    
+
+#atm
+
+balance=int(input("Enter your balance:"))
+
+if balance>=5000:
+    print("You can buy a phone")
+elif balance>=2000:
+    print("You can buy a headphones")
+else:
+    print("Insufficient balance")    
