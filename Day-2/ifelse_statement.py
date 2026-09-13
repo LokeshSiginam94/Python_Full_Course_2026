58cf8a3904b5839a7bf320d6b0446bc6030e5c27
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-13 21:08:00 +0530

    Add ifelse_statements

--- Day-2/ifelse_statement.py
+++ Day-2/ifelse_statement.py
@@ -8,13 +8,7 @@ else:
        
 marks=int(input("Enter your marks: "))
 
-if marks >=90:
-    print("A grade")
-elif marks >=80:
-    print("B grade")
-elif marks >=70:
-    print("C grade")
-elif marks >=60:
-    print("D grade")
+if marks >=50:
+    print("pass")
 else:
-    print("Fail")                  
+    print("fail")
