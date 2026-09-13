d75c32c9f02e4be9f0f783781500cef26b485e95
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-13 21:04:08 +0530

    Add ifelse_statement

---
+++ Day-2/if_statement.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+
+if age >=18:
+    print("You Can Vote")
+print("program ends")
+
+money=500 #money=int(input("Enter your money: "))
+
+if money>=1000:
+    print("You can buy a phone")
+print("Thank You")
---
+++ Day-2/ifelse_statement.py
@@ -0,0 +1,20 @@
+age = int(input("Enter your age:"))
+
+if age >=18:
+    print("Your are eligible to vote")
+else:
+    print("you are not eligible to vote") 
+
+       
+marks=int(input("Enter your marks: "))
+
+if marks >=90:
+    print("A grade")
+elif marks >=80:
+    print("B grade")
+elif marks >=70:
+    print("C grade")
+elif marks >=60:
+    print("D grade")
+else:
+    print("Fail")                  
